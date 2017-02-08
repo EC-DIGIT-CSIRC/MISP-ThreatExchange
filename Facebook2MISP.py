df3c5bf79bbd8b923ec1f0e2089a1ba4faece624
David_Durvaux <david.durvaux@ext.ec.europa.eu>
2017-02-08 16:08:20 +0100

    Some extra tests - start making skeleton

--- Facebook2MISP.py
+++ Facebook2MISP.py
@@ -11,10 +11,15 @@
 
 	To run, set TX_APP_ID and TX_APP_SECRET
 
+	Note - Python API dropped - easier to use web queries
+		API well described here: 
+		https://developers.facebook.com/docs/threat-exchange/reference/apis/threat-indicators/v2.8
+
 	TODO:
 		- credential management
 		- fine control over MISP event creation
 		- switch to Python 3.4 (see warnings)
+		- create a class for Facebook
 """
 import os
 import sys
@@ -27,22 +32,56 @@ import requests
 # Import MISP API
 from pymisp import PyMISP
 
+class FacebookTE():
+	app_id = None
+	app_secret = None
 
-def retrieveMalwareAnalysesLast24h():
-	app_id = os.environ['TX_APP_ID']
-	app_secret = os.environ['TX_APP_SECRET']
-	end_time = int(time.time()) # NOW
-	start_time = end_time - (24 * 3600) # NOW - 24h
+	def __init__(self, app_id, app_secret):
+		self.app_id = app_id
+		self.app_secret = app_secret
+
+
+
+	def retrieveMalwareAnalysesLastNDays(self, numbdays):
+		"""
+			Retrieve the list of malware analysis published
+			for the last numbdays.
+		"""
+		end_time = int(time.time()) # NOW
+		start_time = end_time - numbdays * (24 * 3600)
+		
+		query_params = urllib.urlencode({
+    		'access_token' : self.app_id + '|' + self.app_secret,
+    		'since' : start_time,
+    		'until' : end_time
+    		})
+		
+		r = requests.get('https://graph.facebook.com/v2.8/malware_analyses?' + query_params)
+		
+		print json.dumps(ast.literal_eval(r.text), sort_keys=True,indent=4,separators=(',', ': '))
 	
-	query_params = urllib.urlencode({
-    	'access_token' : app_id + '|' + app_secret,
-    	'since' : start_time,
-    	'until' : end_time
-    	})
 	
-	r = requests.get('https://graph.facebook.com/v2.8/malware_analyses?' + query_params)
+	def retrieveThreatIndicatorsLastNDays(self, numbdays):
+		"""
+			Retrieve the list of published indicators
+			for the last numbdays.
+		"""
+		end_time = int(time.time()) # NOW
+		start_time = end_time - numbdays * (24 * 3600) # NOW - 24h
 	
-	print json.dumps(ast.literal_eval(r.text), sort_keys=True,indent=4,separators=(',', ': '))
+		query_params = urllib.urlencode({
+    		'access_token' : self.app_id + '|' + self.app_secret,
+    		'since' : start_time,
+    		'until' : end_time
+    		})
+		
+		r = requests.get('https://graph.facebook.com/v2.8/threat_indicators?' + query_params)
+		
+		print json.dumps(ast.literal_eval(r.text), sort_keys=True,indent=4,separators=(',', ': '))	
+
+	def retrieveEvent(self, eventid):
+		print("NOT IMPLEMENTED")
+		return None
 
 
 """
@@ -55,7 +94,11 @@ def main():
 		sys.exit(-1)
 
 	# Retrieve event from Facebook
-	retrieveMalwareAnalysesLast24h() #TEST!!
+	fb = FacebookTE(os.environ['TX_APP_ID'], os.environ['TX_APP_SECRET'])
+	fb.retrieveThreatIndicatorsLastNDays(1) # TEST
+
+	# TODO - for each new publisehd Indicators, retrieve full object
+	# then pushed to MISP
 
 	# All done ;)
 	return
